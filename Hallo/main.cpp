f8b70dc76acc575906bccd7cca36eec1247ec58f
Yudo Patriabekti <masYudo@Yudos-MacBook-Pro.local>
2013-03-26 21:31:09 +1000

    Initial Commit

---
+++ Hallo/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Hallo
+//
+//  Created by Yudo Patriabekti on 26/03/13.
+//  Copyright (c) 2013 Yudo Patriabekti. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
